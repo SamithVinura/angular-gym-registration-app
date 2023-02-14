5e1bea0f03894c3451b38d41e4c7b2c61e9fda8d
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-02-14 09:12:46 +0530

    add some external modules

---
+++ src/app/create-registration/create-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-registration',
+  templateUrl: './create-registration.component.html',
+  styleUrls: ['./create-registration.component.scss']
+})
+export class CreateRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
