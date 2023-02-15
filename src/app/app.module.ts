e698076bf7a8a552c67d1790c917b71daf2fe5f8
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-02-15 10:49:41 +0530

    create registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CreateRegistrationComponent } from './create-registration/create-registration.component';
-import { RegistrationListComponent } from './registration-list/registration-list.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CreateRegistrationComponent } from './components/create-registration/create-registration.component';
+import { RegistrationListComponent } from './components/registration-list/registration-list.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +30,15 @@ import { NgConfirmModule } from 'ng-confirm-box';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgToastModule,
-    NgConfirmModule
+    NgConfirmModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/create-registration/create-registration.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-registration',
+  templateUrl: './create-registration.component.html',
+  styleUrls: ['./create-registration.component.scss'],
+})
+export class CreateRegistrationComponent implements OnInit {
+  public packages: string[] = ['Monthly', 'Quartely', 'Yearly'];
+  public gender: string[] = ['Male', 'Female'];
+  public importantList: string[] = [
+    'Toxic Fat reduction',
+    'Energy and Endurance',
+    'Building Lean Muscle',
+    'Healthier Digestive System',
+    'Sugar Craving Body',
+    'Fitness',
+  ];
+
+  public registerForm!:FormGroup
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm =this.fb.group({
+      firstName:[''],
+      lastName:[''],
+      email:[''],
+      mobile:[''],
+      weight:[''],
+      height:[''],
+      bmi:[''],
+      bmiResult:[''],
+      gender:[''],
+      requireTrainer:[''],
+      package:[''],
+      important:[''],
+      haveGymBefore:[''],
+      enquiryDate:['']
+
+    })
+  }
+
+  submit(){
+    console.log(this.registerForm.value)
+  }
+}
